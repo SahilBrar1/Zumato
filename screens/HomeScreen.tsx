db7b0099407b34533cae6c71b1d3ef5da4283a74
jyotidhiman4463 <jyotidhiman4463@gmail.com>
2025-04-11 10:35:35 +0530

    unknown

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import { useNetInfo } from '../components/NetInfoContext';
-import {Text} from 'react-native'
+
 import HotelList from "../components/HotelList";
 
 const HomeScreen = ({ navigation }: any) => {
-  const { isConnected } = useNetInfo();
-  return (<>
-  <HotelList />
-    <Text>Network Status: {isConnected ? 'Online' : 'Offline'}</Text>
-  </>
-      
-  );
+  return <HotelList />;
 };
 
 export default HomeScreen;
