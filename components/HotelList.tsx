74beb679ba1a3df821e0c00209b1caf2c3ad1e78
jyotidhiman4463 <jyotidhiman4463@gmail.com>
2025-04-11 10:31:25 +0530

    unknown

--- components/HotelList.tsx
+++ components/HotelList.tsx
@@ -96,6 +96,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     marginBottom: 10,
+  
     
   
   },
--- components/ItemComponent.tsx
+++ components/ItemComponent.tsx
@@ -19,5 +19,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 10,
     
+    
   },
 });
